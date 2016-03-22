23c4da013168ac5d08e6383a98b7bec2639b7f4e
Fiware ULPGC <fiware@ulpgc.es>
2016-03-22 08:03:06 +0000

    fixed authors text

--- client/js/myReviews.js
+++ client/js/myReviews.js
@@ -3,7 +3,7 @@
  * myReviews.js
  * Copyright(c) 2016 Universidad de Las Palmas de Gran Canaria
  * Authors:
- *   Jaisiel Santana <jaisiel@gmail.com>,
+ *   Jaisiel Santana <jaisiel@gmail.com>
  *   Alejandro Sánchez <alemagox@gmail.com>
  *   Pablo Fernández <pablo.fernandez@ulpgc.es>
  * MIT Licensed
