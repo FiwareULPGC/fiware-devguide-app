eacb032deae7a6b66c0fdccf8b72a9c79aa2463b
Fiware ULPGC <fiware@ulpgc.es>
2016-05-16 07:28:16 +0100

    Show message test

---
+++ client/spec/test_utils.js
@@ -0,0 +1,78 @@
+var utils = require('../js/utils.js')
+var utils_config = {
+  'file':'file://'+__dirname+'/config_utils.html',
+  'done': function(){}
+};
+var jsdom = require("jsdom").jsdom;
+require('mocha-jsdom')
+
+
+
+describe('Testing utils module', function () {
+  
+  it('capitalizes a string', function() {
+    var result = utils.capitalize('foobar');   
+    expect(result).to.be.a('string').and.equal('FOOBAR');
+  });
+
+  describe('Testing showMessage function', function() {
+
+    beforeEach(function() {
+
+      var doc = jsdom("<div class='container-fluid'>"+
+          "<div id='topMenu'></div>" +
+          "<div id='sibling1'></div>" +
+        "</div>", {});
+      global.window = doc.defaultView;
+      global.document = window.document;
+    });
+
+    afterEach(function() {
+      while (window.firstChild) {
+        window.removeChild(myNode.firstChild);
+      }
+      delete global.window;
+      delete global.document;
+    })
+
+
+    it('test alert-danger message', function() {
+      var message = 'test 1';
+      utils.showMessage(message, 'alert-danger');
+
+      var navBar = document.getElementById('topMenu');
+      var messageDiv = navBar.nextSibling;
+      
+      expect(messageDiv).to.have.property('className').that.is.a('string').to.contain('alert').to.contain('fade').to.contain('in').to.contain('alert-danger');
+      expect(messageDiv).to.have.property('textContent').that.is.a('string').to.be.equal(message+'X')
+    
+
+    })
+
+    it('test alert-warning message', function() {
+      var message = 'test 2';
+      utils.showMessage(message, 'alert-warning');
+
+      var navBar = document.getElementById('topMenu');
+      var messageDiv = navBar.nextSibling;
+
+      expect(messageDiv).to.have.property('className').that.is.a('string').to.contain('alert').to.contain('fade').to.contain('in').to.contain('alert-warning');
+      expect(messageDiv).to.have.property('textContent').that.is.a('string').to.be.equal(message+'X')
+
+    })
+
+    it('test default message', function() {
+      var message = 'test 3';
+      utils.showMessage(message);
+
+      var navBar = document.getElementById('topMenu');
+      var messageDiv = navBar.nextSibling;
+
+      expect(messageDiv).to.have.property('className').that.is.a('string').to.contain('alert').to.contain('fade').to.contain('in').to.contain('alert-warning');
+      expect(messageDiv).to.have.property('textContent').that.is.a('string').to.be.equal(message+'X')
+
+    })
+  })
+
+});
+  
